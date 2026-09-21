fe86ba70f4ea8a1ea1f79b776bd38038a5388ea6
Jie <spssspss0712@hotmail.com>
2026-09-21 00:13:56 -0700

    docs: add docstrings to endpoints

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ checks = {}
 
 @app.get("/health")
 def health_check():
+    """Check API status return it as ok."""
     return {"status": "ok"}
 
 
@@ -18,6 +19,7 @@ class CheckRequest(BaseModel):
 
 @app.post("/checks", status_code=status.HTTP_202_ACCEPTED)
 def create_check(request: CheckRequest):
+    """Create a check job and return it as queued."""
     url = request.url
     check_id = str(uuid4())
     checks[check_id] = {
@@ -31,6 +33,7 @@ def create_check(request: CheckRequest):
 
 @app.get("/checks/{check_id}")
 def get_check(check_id: str):
+    """Return check if id exists, or return 404 if id is not found"""
     if check_id not in checks:
         raise HTTPException(status_code=404, detail="ID not found")
     return checks[check_id]
