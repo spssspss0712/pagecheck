e3cb5796e3d2e89b1d19317ffd4f23deb1674c72
Jie <spssspss0712@hotmail.com>
2026-09-23 12:28:15 -0700

    feat: add meta description check

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,14 @@ from bs4 import BeautifulSoup
 
 def analyze_page(html_page: str) -> dict:
     soup = BeautifulSoup(html_page, "html.parser")
+    meta = soup.find("meta", attrs={"name": "description"})
+    result = {"has_title": True, "has_meta_description": True}
 
     if soup.title is None or soup.title.get_text(strip=True) == "":
-        return {"has_title": False}
-    return {"has_title": True}
+        result["has_title"] = False
+
+    content = meta.get("content") if meta else None
+    if content is None or content.strip() == "":
+        result["has_meta_description"] = False
+
+    return result
--- test_analyzer.py
+++ test_analyzer.py
@@ -21,3 +21,36 @@ def test_analyze_page_with_empty_title_return_false():
 def test_analyze_page_with_space_title_return_false():
     result = analyze_page("<html><head><title>     </title></head><body></body></html>")
     assert result["has_title"] is False
+
+
+def test_analyze_page_with_meta_description_return_true():
+    result = analyze_page(
+        "<html><head><meta name='description' content='this is content'></head><body></body></html>"
+    )
+    assert result["has_meta_description"] is True
+
+
+def test_analyze_page_without_meta_return_false():
+    result = analyze_page("<html><head></head><body></body></html>")
+    assert result["has_meta_description"] is False
+
+
+def test_analyze_page_with_meta_no_description_with_content_return_false():
+    result = analyze_page(
+        "<html><head><meta name='title' content='this is content'></head><body></body></html>"
+    )
+    assert result["has_meta_description"] is False
+
+
+def test_analyze_page_with_meta_description_no_content_return_false():
+    result = analyze_page(
+        "<html><head><meta name='description'></head><body></body></html>"
+    )
+    assert result["has_meta_description"] is False
+
+
+def test_analyze_page_with_meta_description_with_empty_content_return_false():
+    result = analyze_page(
+        "<html><head><meta name='description' content='     '></head><body></body></html>"
+    )
+    assert result["has_meta_description"] is False
